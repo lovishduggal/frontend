cef44cbadae3fefd7ea72298ef178eef6a73d0db
Lovish Duggal <lovishduggal11@gmail.com>
2024-03-02 11:16:43 +0530

    Deployment 1.1

--- src/app/utils/axiosInstance.js
+++ src/app/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const Base_URL = 'http://localhost:8080/api/v1';
+const Base_URL = 'https://server-84yt.onrender.com/api/v1';
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = Base_URL;
